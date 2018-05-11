6491100cada093e6697268e9ada4bf901e05dacc
ZeMing <zeming.liu@toursforfun.cn>
2018-05-11 10:49:15 +0800

    monitor init

---
+++ src/main/java/com/tff/monitor/TffMonitorApplication.java
@@ -0,0 +1,12 @@
+package com.tff.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TffMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TffMonitorApplication.class, args);
+	}
+}
